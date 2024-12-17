f7dc89fba242a6e6ef7282a17048535bd02cb8a2
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2024-12-17 17:21:22 +0500

    add at shadcn ui dashboard block

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,17 +6,17 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar";
+} from "@/components/ui/sidebar"
 
-export default function Home() {
+export default function Page() {
   return (
-    <SidebarProvider>
+    (<SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b">
@@ -47,6 +47,6 @@ export default function Home() {
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
       </SidebarInset>
-    </SidebarProvider>
+    </SidebarProvider>)
   );
 }
