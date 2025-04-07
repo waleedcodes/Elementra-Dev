34b0effc2dd7f373b2fce41b2ea406d5fecc7165
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2025-04-07 12:31:53 +0500

    fix SidebarTrigger Icon for dics

--- components/ui/sidebar.jsx
+++ components/ui/sidebar.jsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
-import { PanelLeft } from "lucide-react"
+import { ArrowLeft, PanelLeft } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -226,13 +226,13 @@ const SidebarTrigger = React.forwardRef(({ className, onClick, ...props }, ref) 
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-10 w-10", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
       }}
       {...props}>
-      <PanelLeft />
+      <ArrowLeft />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>)
   );
